1c018590a5b415a2e7f39d940aed3931e3ca3805
GarifullinII <ildaridrisovich@gmail.com>
2024-01-04 23:56:26 +0400

    add home_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_pro/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('Hello, friend')
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
     );
   }
 }
